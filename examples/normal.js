964c3c8db8163000c504504ea688d449b0c8309f
jrvieira <github@jrvieira.com>
2018-01-26 17:22:25 +0000

    Fixed negative sign slice

---
+++ examples/normal.js
@@ -0,0 +1,12 @@
+const graph = require('../lib/graphene')
+
+// Standard Normal variate using Box-Muller transform.
+function boxmuller() {
+	
+	let u = Math.random()
+	let v = Math.random()
+
+	return Math.floor(10 * Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v ))
+}
+
+graph(boxmuller, Number(process.argv[2]) || 1000000)

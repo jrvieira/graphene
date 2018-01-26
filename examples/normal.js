766b101c6b80b0edc8216991c6c5620793a28cd4
jrvieira <github@jrvieira.com>
2018-01-26 17:39:26 +0000

    Updated examples

--- examples/normal.js
+++ examples/normal.js
@@ -6,7 +6,7 @@ function boxmuller() {
 	let u = Math.random()
 	let v = Math.random()
 
-	return Math.floor(10 * Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v ))
+	return Math.floor(10 * Math.sqrt( - 2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v ))
 }
 
 graph(boxmuller, Number(process.argv[2]) || 1000000)
